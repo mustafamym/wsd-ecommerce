dbda521a866a887535a66e600efa859bbb1739c3
Gulam Mustafa <mustafamym@gmail.com>
2024-01-31 01:55:45 +0600

    initial framework setup
    initial framework setup

---
+++ src/main/java/com/wsd/WsdEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.wsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsdEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wsd/WsdEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
